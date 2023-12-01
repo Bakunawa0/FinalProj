4c7ab45341d52d0611ed8be8b6a77ff27241979a
Bakunawa0 <danieljuliandado4@gmail.com>
2023-12-01 17:11:29 +0800

    Initial skeleton

---
+++ FinalProject.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FinalProject extends JFrame {
+    public FinalProject() {
+        
+    }
+}
