2fbcc4d023a43928bf346fa4cea22527ffe5dff6
Bakunawa0 <danieljuliandado4@gmail.com>
2023-12-05 23:07:06 +0800

    Got bored made encrypt/decrypt UI

--- FinalProject.java
+++ FinalProject.java
@@ -35,6 +35,22 @@ public class FinalProject extends JFrame {
         // Need to reference the parent of these UI components later
         FinalProject parent = this;
 
+        // open encrypt menu
+        encryptItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                final EDWindow eDWindow = new EDWindow(parent, true, key);
+                eDWindow.setVisible(true);
+            }
+        });
+        
+        // open decrypt menu
+        decryptItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                final EDWindow eDWindow = new EDWindow(parent, false, key);
+                eDWindow.setVisible(true);
+            }
+        });
+
         // settings option opens SettingsDialog
         settingsItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -59,6 +75,37 @@ public class FinalProject extends JFrame {
 
     public static void main(String[] args) {
         FinalProject fp = new FinalProject();
+    }
+}
+
+class EDWindow extends JDialog {
+    public EDWindow(JFrame parent, boolean encryptMode, String key) {
+        super(parent, (encryptMode ? "Encrypt" : "Decrypt") + " Messages", true);
+        setLocationRelativeTo(parent);
+        setResizable(false);
+        setSize(400, 300);
+        
+        String keyString = (encryptMode ? "Encryption" : "Decryption") + " Key";
+
+        JLabel messageLabel = new JLabel("Message");
+        JLabel resultLabel = new JLabel("Result");
+        JTextArea messageArea = new JTextArea();
+        JTextArea resultArea = new JTextArea();
+        resultArea.setEditable(false);
+        JLabel keyLabel = new JLabel(keyString);
+        JTextField keyField = new JTextField(key);
+        JButton eDButton = new JButton(encryptMode ? "Encrypt" : "Decrypt");
+
+        add(messageLabel);
+        add(resultLabel);
+        add(new JScrollPane(messageArea));
+        add(new JScrollPane(resultArea));
+        add(keyLabel);
+        add(new JSeparator());
+        add(keyField);
+        add(new JSeparator());
+        add(eDButton);
+        setLayout(new GridLayout(0, 3));
     }
 }
 
